d706ad417be4374813c856fa5d0c81c1a4e0bf9b
Alex Matthiessen <AlexMac@guest-wireless-upc-1603-10-120-063-057.usc.edu>
2018-03-29 20:49:22 -0700

    Plots

--- Analysis_Code/analysis.py
+++ Analysis_Code/analysis.py
@@ -10,13 +10,33 @@ years= ['2002','2003','2004','2005','2006','2007','2008','2009','2010','2011','2
 asianm = []
 asianw= []
 
+latinom = []
+latinow= []
+
 for year in years:
     
     asianm.append(DF.loc['A, Men','{}_median_weekly_earnings'.format(year)])
     asianw.append(DF.loc['A, Women','{}_median_weekly_earnings'.format(year)])
+
+    latinom.append(DF.loc['H/L, Men','{}_median_weekly_earnings'.format(year)])
+    latinow.append(DF.loc['H/L, Women','{}_median_weekly_earnings'.format(year)])
+
+    
+
     
 years = list(map(int, years))
+plt.title('Wages - Asian Men / Women')
+diffa= pd.Series(asianm) - pd.Series(asianw)
 
+plt.scatter(years, diffa, color = 'pink')
 plt.scatter(years, asianm)
 plt.scatter(years, asianw)
+plt.show()
+
+plt.title('Wages - Latino Men / Women')
+diffl= pd.Series(asianm) - pd.Series(asianw)
+
+plt.scatter(years, diffl, color = 'pink')
+plt.scatter(years, latinom)
+plt.scatter(years, latinow)
 plt.show()
